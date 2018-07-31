ea7f3e8096490d931b7885d10d5365ffe6a430d2
Anderson Mendes <andersonmendes2k@gmail.com>
2018-07-31 20:05:28 -0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,31 @@
 <?php
 
+use App\Http\Middleware\CheckTasks;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::group(['middleware'=>['alerttasks','auth']], function(){
+Route::group(['middleware' => ['alerttasks', 'auth']], function() {
 
-Route::get('/', function () {
-    return view('home');
+    Route::get('/', function () {
+        return view('helpers');
+    });
+    
+    Route::get('clients/pdf', 'ExtraActions\ClientPdf');
+    Route::resource('clients', 'ClientController');
+    
+    Route::get('tasks/add/{id}', 'ToDoTasksController@store');
+    Route::get('tasks/delete/{id}', 'ToDoTasksController@destroy')->middleware(CheckTasks::class);
+
 });
-
-Route::get('tasks/add/{id}', 'toDoTasks@store');
-
-Route::get('tasks/delete/{id}', 'toDoTasks@destroy')->middleware('checktasks');
-
-
-Route::resource('clients', 'ClientController');
-
-});
-
-
-
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
